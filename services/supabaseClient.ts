7c5bdb1492601cfc8735dddd738bf491f9ae8153
djflowerz <49757843+djflowerz@users.noreply.github.com>
2025-12-04 12:36:41 +0300

    Update supabaseClient.ts

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -1,5 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from '../constants';
+
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const isValidConfig =
   SUPABASE_URL &&
